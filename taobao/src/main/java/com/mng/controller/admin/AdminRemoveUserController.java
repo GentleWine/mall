dae0114201fdd6f3a7826560649da1b83246b678
TCreopargh <TCreopargh@gmail.com>
2021-04-27 00:55:39 +0800

    refactored admincontroller

--- taobao/src/main/java/com/mng/controller/admin/AdminRemoveUserController.java
+++ taobao/src/main/java/com/mng/controller/admin/AdminRemoveUserController.java
@@ -3,33 +3,41 @@ package com.mng.controller.admin;
 import com.alibaba.fastjson.JSONObject;
 import com.mng.bean.UserRemoveBody;
 import com.mng.controller.account.AccountControllerBase;
+import com.mng.entity.User;
 import com.mng.util.JsonBuilder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+import java.util.Optional;
 
 public class AdminRemoveUserController extends AccountControllerBase {
     @RequestMapping(value = "/admin/remove-user", method = RequestMethod.POST)
     public JSONObject resolve(HttpServletRequest request, @ModelAttribute("user") UserRemoveBody body) {
-        Integer userId = body.getUserId();
-        String username = body.getUsername();
-        if (userId != null) {
-            userRepository.deleteById(userId);
-            return JsonBuilder.newObject()
-                    .put("success", true)
-                    .buildAsJsonObject();
-        }
-        if (username != null) {
-            userRepository.deleteAll(userRepository.findByUsername(username));
-            return JsonBuilder.newObject()
-                    .put("success", true)
-                    .buildAsJsonObject();
-
-        }
-        return JsonBuilder.newObject()
+        final Integer userId = body.getUserId();
+        final String username = body.getUsername();
+        final JSONObject jsonSuccess = JsonBuilder.newObject()
+                .put("success", true)
+                .buildAsJsonObject();
+        final JSONObject jsonFail = JsonBuilder.newObject()
                 .put("success", false)
                 .buildAsJsonObject();
+        if (userId != null) {
+            Optional<User> userToRemove = userRepository.findById(userId);
+            if (userToRemove.isPresent()) {
+                userRepository.delete(userToRemove.get());
+                return jsonSuccess;
+            }
+        }
+        if (username != null) {
+            List<User> usersToRemove = userRepository.findByUsername(username);
+            if (!usersToRemove.isEmpty()) {
+                userRepository.deleteAll(usersToRemove);
+                return jsonSuccess;
+            }
+        }
+        return jsonFail;
     }
 }
