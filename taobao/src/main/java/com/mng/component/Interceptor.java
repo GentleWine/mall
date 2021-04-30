b178221456b94239088a85b07ae46c3dcfd7dbf1
TCreopargh <TCreopargh@gmail.com>
2021-04-30 10:54:26 +0800

    Interceptor no longer intercepts admin pages

--- taobao/src/main/java/com/mng/component/Interceptor.java
+++ taobao/src/main/java/com/mng/component/Interceptor.java
@@ -11,6 +11,13 @@ import javax.servlet.http.HttpServletResponse;
 public class Interceptor implements HandlerInterceptor {
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+
+        // Admin Panel
+        if(request.getRequestURL().toString().matches("^(https?://)?[a-zA-Z0-9.-]+/admin(/\\S+)?/?$")) {
+            Log.i("Request URL on Admin Section: " + request.getRequestURL());
+            Log.i("Request Method on Admin Section: " + request.getMethod());
+            return true;
+        }
         if (request.getSession().getAttribute("phone") == null) {
             Log.i("Request URL: " + request.getRequestURL());
             Log.i("Request Method: " + request.getMethod());
