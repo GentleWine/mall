9b2dbe23b4ce271266d89ca6f839ff045ce7a448
minangong <82138101+minangong@users.noreply.github.com>
2021-05-04 13:48:57 +0800

    commodityRepository delete

--- taobao/src/main/java/com/mng/repository/CommodityRepository.java
+++ taobao/src/main/java/com/mng/repository/CommodityRepository.java
@@ -2,10 +2,14 @@ package com.mng.Repository;
 
 import com.mng.entity.Commodity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 public interface CommodityRepository extends JpaRepository<Commodity, Integer> {
     List<Commodity> findByShopid(Integer shopid);
     List<Commodity> findByShopidAndName(Integer shopid,String name);
+    List<Commodity> findByComid(Integer comid);
+    @Transactional
+    void deleteByComid(Integer comid);
 }
