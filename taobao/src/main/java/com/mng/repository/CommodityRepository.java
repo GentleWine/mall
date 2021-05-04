4d2505439d9ddd07f9364716f8dc5654e2d73aca
minangong <82138101+minangong@users.noreply.github.com>
2021-05-04 17:41:53 +0800

    amount

--- taobao/src/main/java/com/mng/repository/CommodityRepository.java
+++ taobao/src/main/java/com/mng/repository/CommodityRepository.java
@@ -2,6 +2,9 @@ package com.mng.Repository;
 
 import com.mng.entity.Commodity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -12,4 +15,10 @@ public interface CommodityRepository extends JpaRepository<Commodity, Integer> {
     List<Commodity> findByComid(Integer comid);
     @Transactional
     void deleteByComid(Integer comid);
+
+    @Modifying
+    @Transactional
+    @Query(value="update commodity  set amount =:newamount where comid =:comid",nativeQuery = true)
+    public int updateAmountByComid(@Param("newamount") Double newamount, @Param("comid") Integer comid);
+
 }
