7fdfb79aba72c8c7e62cc7c10d046f8048c7e0eb
bleedshadow <41578112+bleedshadow@users.noreply.github.com>
2021-06-05 20:50:37 +0800

    Add files via upload
    订单实体 对应orders数据库

---
+++ taobao/src/main/java/com/mng/entity/Orders.java
@@ -0,0 +1,38 @@
+package com.mng.entity;
+
+import lombok.Data;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.Date;
+
+@DynamicInsert
+@DynamicUpdate
+@Data
+@Entity
+@Table(name = "orders")
+public class Orders implements Serializable {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer orderid;
+
+    private Integer userid;
+
+    private Integer comid;
+
+    private Integer number;
+
+    private Double payment;
+
+    private Integer paymenttype;
+
+    private Integer status;
+
+    private Date paymenttime;
+
+    public Orders() {
+
+    }
+}
