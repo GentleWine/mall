50ce4955599d47e3d5aa438806fdf1614e868829
bleedshadow <41578112+bleedshadow@users.noreply.github.com>
2021-06-09 21:35:33 +0800

    Add files via upload

---
+++ taobao/src/main/java/com/mng/controller/orders/UserOrdersController.java
@@ -0,0 +1,30 @@
+package com.mng.controller.orders;
+
+
+import com.mng.entity.Order;
+import com.mng.entity.User;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import org.stringtemplate.v4.ST;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.List;
+
+@Controller
+public class UserOrdersController extends OrderControllerBase {
+    @RequestMapping(value = "/user/orders", method = RequestMethod.GET)
+    public String seller(Model model, HttpServletRequest request, @RequestParam("username") String name) {
+        //Integer
+        //Integer shopid=Integer.parseInt(request.getSession().getAttribute("shopid").toString());
+        List<User> users=userRepository.findByUsername(name);
+        User user=users.get(0);
+        List<Order> ordersList = orderRepository.findByUserid(user.getUserid());
+
+        model.addAttribute("allProducts", ordersList);
+        return "userorders";
+    }
+}
