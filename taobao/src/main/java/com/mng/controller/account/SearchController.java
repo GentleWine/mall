dc864e6141b998d4487837e615b76138b594e05f
bleedshadow <41578112+bleedshadow@users.noreply.github.com>
2021-06-10 17:21:03 +0800

    搜索添加了功能

--- taobao/src/main/java/com/mng/controller/account/SearchController.java
+++ taobao/src/main/java/com/mng/controller/account/SearchController.java
@@ -1,10 +1,13 @@
 package com.mng.controller.account;
 
+import com.fasterxml.jackson.databind.JsonSerializer;
 import com.mng.annotation.LoginRequired;
 import com.mng.controller.shop.ShopControllerBase;
+import com.mng.domain.ItemDomain;
 import com.mng.domain.KindDomain;
 import com.mng.domain.SellerDomain;
 import com.mng.domain.UserDomain;
+import com.mng.entity.Commodity;
 import com.mng.entity.Shop;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -17,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
@@ -31,17 +35,18 @@ public class SearchController extends ShopControllerBase {
         Page<Shop> pages = shopRepository.findAll(pageable);
         List<Shop> shops = pages.getContent();
         UserDomain user = new UserDomain();
+
         Object username = request.getSession().getAttribute("username");
         if (username != null) {
             user.setName(username.toString());
         }
-
+        user.setSpent_money(userRepository.findByUsername(username.toString()).get(0).getSpentmoney());
         List<SellerDomain> sellers = getSellers(shops);
 
         //删除name不是content的元素
         for (SellerDomain i : sellers) {
             List<KindDomain> kinds = i.getKinds();
-            //System.out.println(kinds.toString());
+
             for (int j = 0; j < kinds.size(); j++) {
                 //System.out.println(kinds.get(j));
                 if (!Objects.equals(kinds.get(j).getName(), content)) {
@@ -51,7 +56,34 @@ public class SearchController extends ShopControllerBase {
             }
             i.setKinds(kinds);
         }
-
+        sellers.removeIf(i -> i.getKinds().size() == 0);
+        if (sellers.size()==0){
+            SellerDomain i = new SellerDomain();
+            i.setName("search result");
+            i.setAddress("flexible");
+            List<KindDomain> kinds=new ArrayList<>();
+            KindDomain k =new KindDomain();
+            kinds.add(k);
+            k.setName("search result");
+            List<ItemDomain>  items = new ArrayList<>();
+            List<Commodity>  commoditys=commodityRepository.findByName(content);
+            for(Commodity temp : commoditys){
+                ItemDomain item=new ItemDomain();
+                item.setAddress(shopRepository.findByShopid(temp.getShopid()).get(0).getAddress());
+                item.setAmount(temp.getAmount());
+                item.setId(temp.getComid());
+                item.setImgUrl(temp.getMainimage());
+                item.setItem_title(temp.getName());
+                item.setPrice(temp.getPrice());
+                item.setSeller_info(temp.getDetail());
+                items.add(item);
+            }
+            k.setItems(items);
+            i.setKinds(kinds);
+            sellers.add(i);
+            //sellers.add(new SellerDomain());
+        }
+        System.out.println(sellers.toString());
         model.addAttribute("sellers", sellers);
         model.addAttribute("user", user);
         //System.out.println(sellers);
